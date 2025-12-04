715ae6340294a31acd97ff028c843f62885a6cd7
andrea-desantis <andrea.desantis290@gmail.com>
2025-12-05 00:25:25 +0100

    feat: lista

--- react-form/src/components/AppMain.jsx
+++ react-form/src/components/AppMain.jsx
@@ -5,32 +5,40 @@ import { useState } from 'react';
 export default function AppMain(){
     const [newItem, setNewItem] = useState('');
 
+    const [item, setItem] = useState([])
 
+
+
+    function handlerSubmit(event){
+      event.preventDefault(); 
+      setItem([...item, newItem]); 
+      setNewItem('');
+    };
     return (
         <main>
 
             <ul>
-                <li>
-                    <p>ciao</p>
-                </li>
-                <li>
-                    <p>no</p>
-                </li>
+                {item.map((item, index) => (
+                    <li key={index}>
+                        <p>{item}</p>
+                    </li>
+                ))}
             </ul>
 
 
-            <form>
-                <h1>{newItem}</h1>
-                <h1>{setNewItem}</h1>
+            <form onSubmit={handlerSubmit}>
+                
+                
                 <input 
                     type="text" 
                     value= {newItem}
                     onChange={(event) => {
                         const newNewItem = event.target.value;
-                        setNewItem(newNewItem);
-                    
-                    }} 
+                        setNewItem(newNewItem);     
+                    }}
                 />
+
+                <button type='submit'>invia</button>
             </form>
         </main>
     );
